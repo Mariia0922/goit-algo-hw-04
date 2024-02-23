4abd3f989a9bfa320f8466cba3ce99c252a2a615
Mariia0922 <149525452+Mariia0922@users.noreply.github.com>
2024-02-23 17:56:40 +0100

    path1.py

--- path1.py
+++ path1.py
@@ -1,8 +1,6 @@
-# Імпортуємо список cats з модуля cats_info
 from cats_info import cats
 
 def get_cats_info(path):
-    # Параметр 'path' не використовується, оскільки дані вже завантажені в пам'ять
     return cats
 
 cats_info = get_cats_info('шлях/до/файлу/не/використовується')
